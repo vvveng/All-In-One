89e196eecde35a06ced330229f4111c379609fb0
vvveng <charlessunlc@foxmail.com>
2023-05-26 21:23:02 +0800

    快捷更改

---
+++ JS/Jiejing_Auto_get.js
@@ -0,0 +1,23 @@
+// 使用时先打开模块然后再看应用点击签到
+
+const APPName = '捷径社区参数'
+const url = $request.url
+
+const userKey = 'A_JieJing_User'
+const userVal = $request.headers['x-user']
+
+const tokenKey = 'A_JieJing_token'
+const tokenVal = $request.headers['x-token']
+
+if (tokenVal) {
+  let token = $persistentStore.write(tokenVal, tokenKey)
+  let user = $persistentStore.write(userVal, userKey)
+  if (token) {
+    let msg = `${APPName}`
+    $notification.post(msg, '写入成功', '详见日志')
+    console.log(msg)
+    console.log(tokenVal,userVal)
+  }
+}
+
+$done({})
