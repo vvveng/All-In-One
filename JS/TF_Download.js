7d59595fd6932aa634b9aed7e39a796461e25c7a
vvveng <charlessunlc@gmail.com>
2023-06-26 02:09:01 +0800

    快捷更改

--- JS/TF_Download.js
+++ JS/TF_Download.js
@@ -1,7 +1,7 @@
 $request.headers = formatHeaders($request.headers)
 function formatHeaders(h) {
     return Object.keys(h).reduce((t, i) => (t[i.toLowerCase()] = h[i], t), {})
-}
+};
 let app = JSON.parse($request.body);
 app.storefrontId = '143441-19,29';
 $done({body:JSON.stringify(app)});
